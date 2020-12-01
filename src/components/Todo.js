e53d0b4a9022ba05ae1a8e5696dbcde05b256ff2
ndossett <niki.m.dossett@gmail.com>
2020-11-30 19:36:54 -0500

    finished todo form

---
+++ src/components/Todo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Todo = props => {
+    const handleClick = () => {
+        props.handleToggle(props.todo.id)
+    }
+
+    return (
+        <div onClick={handleClick} className={`todo${props.todo.completed ? 'completed' : ''}`}>
+            <p>{props.todo.name}</p>
+        </div>
+    )
+}
+
+export default Todo
